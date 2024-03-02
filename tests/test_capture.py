45feaf051141b6c8f5e593270abd2633d5945609
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-03-02 12:45:07 +0300

    Fixed test assert statement

--- tests/test_capture.py
+++ tests/test_capture.py
@@ -2,5 +2,5 @@ from src.capture import capture
 
 def test_capture() -> None:
     sound = capture()
-    assert isinstance(sound, float)
+    assert isinstance(sound, list)
     assert all(isinstance(_, float) for _ in sound)
